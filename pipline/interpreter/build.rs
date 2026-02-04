929fe30cf72c95d61e755c5d9fbe61b77dd3fdce
Sunken Ahn <devsanso@outlook.com>
2026-02-04 23:43:32 +0900

    build 스크립트 수정

--- pipline/interpreter/build.rs
+++ pipline/interpreter/build.rs
@@ -2,7 +2,6 @@ use std::env;
 use std::process::Command;
 
 fn main() {
-    // 현재 파이썬이 사용하는 lib 디렉토리 찾아오기 (예: venv)
     let python = env::var("PYO3_PYTHON").unwrap_or_else(|_| "python3".to_string());
 
     let output = Command::new(&python)
@@ -19,12 +18,9 @@ fn main() {
 
     let library = String::from_utf8(output.stdout).unwrap().trim().to_string();
 
-    // 링커에 라이브러리 검색 경로 전달
     println!("cargo:rustc-link-search=native={}", library.replace("lib",""));
 
-    // 필요 시 직접 링크할 라이브러리도 지정 (플랫폼에 따라 이름 조정)
-    println!("cargo:rustc-link-lib=dylib={}", "python3.12");
+    println!("cargo:rustc-link-lib=dylib={}", library.replace("lib",""));
 
-    // build.rs 변경 시 다시 실행
     println!("cargo:rerun-if-changed=build.rs");
 }
